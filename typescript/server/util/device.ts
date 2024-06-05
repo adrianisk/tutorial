68b62b4ffc27e6c38c1756e86c43c2232305ef17
Adrian Kreuziger <adrian@manifestdata.ai>
2024-06-04 18:22:01 -0700

    Capture Android & iPhone device info

--- typescript/server/util/device.ts
+++ typescript/server/util/device.ts
@@ -3,11 +3,18 @@ import parser from "ua-parser-js";
 export enum DEVICE {
   WEB = "web",
   MOBILE = "mobile",
+  ANDROID = "android",
+  IPHONE = "iphone",
 }
 
 export function getDeviceFromUserAgent(userAgent: string): DEVICE {
   const uaParsed = parser(userAgent);
   if (uaParsed.device.type === "mobile") {
+    if (uaParsed.os.name === "iOS") {
+      return DEVICE.IPHONE;
+    } else if (uaParsed.os.name === "Android") {
+      return DEVICE.ANDROID;
+    }
     return DEVICE.MOBILE;
   }
   return DEVICE.WEB;
